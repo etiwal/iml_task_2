0cc05b6aacf4ed0ae143b8c3641db221e9d3862a
etiwal <55783756+etiwal@users.noreply.github.com>
2020-04-20 16:11:37 +0200

    added stats csv

--- main.py
+++ main.py
@@ -5,21 +5,32 @@ from sklearn.svm import LinearSVR
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import SimpleImputer, IterativeImputer
 
-def impute_data(data, impute_strategy = 'SimpleImputer'):
+def impute_data(input_df, impute_strategy = 'SimpleImputer'):
     if impute_strategy == 'SimpleImputer':
         imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-        data = imp.fit_transform(data)
+        df = imp.fit_transform(input_df, input_df)
 
-    return data
+    return pd.DataFrame(df, columns=list(input_df.columns.values))
 
 df_train_features = pd.read_csv('train_features.csv')
 #df_train_labels = pd.read_csv('train_labels.csv')
 #df_test_features = pd.read_csv('test_features.csv')
 
-pids_train, pids_validate = np.array(df_train_features['pid'].unique()), 0
+pids_train, pids_validate = df_train_features['pid'].unique(), 0
 
-train_features_imputed = impute_data(np.array(df_train_features))
+# build statistics
+# train_features_stats = pd.DataFrame()
+# train_features_stats['nan_count'] = df_train_features.isna().sum()
+# train_features_stats['mean'] = df_train_features.mean()
+# train_features_stats['std'] = df_train_features.std()
+# train_features_stats = train_features_stats.T
+# train_features_stats.to_csv('train_features_stats.csv', sep=',', float_format='%.3f', encoding='utf-8', index=False)
+
+
+
+train_features_imputed = impute_data(df_train_features)
 print(train_features_imputed)
+train_features_imputed.to_csv('train_features_imputed.csv', sep=',', float_format='%.3f', encoding='utf-8', index=False)
 
 # dict structure
 #train_features = {'11402': {'age': 22, '1':{'EtCO2': 123, 'etc':133}, '2':{'EtCO2': 234, 'etc':432}}}
